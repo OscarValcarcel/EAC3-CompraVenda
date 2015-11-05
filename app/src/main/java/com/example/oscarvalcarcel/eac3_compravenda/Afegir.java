2ba178d0a6969669c63f64dd8509576473d9b60f
Twisted2 <oscarvalcarcelmorales@hotmail.com>
2015-11-05 19:08:10 +0100

    algunos cambios

--- app/src/main/java/com/example/oscarvalcarcel/eac3_compravenda/Afegir.java
+++ app/src/main/java/com/example/oscarvalcarcel/eac3_compravenda/Afegir.java
@@ -1,8 +1,13 @@
 package com.example.oscarvalcarcel.eac3_compravenda;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,11 +17,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class Afegir extends AppCompatActivity {
 
     TextView titol;
     TextView preu;
     ImageView imatge;
+    private int REQ_CAMERA = 0;
+    private String uploadImagePath = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +43,10 @@ public class Afegir extends AppCompatActivity {
         imatge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, 1);
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                File f = new File(android.os.Environment.getExternalStorageDirectory(), "Items");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                startActivityForResult(intent, REQ_CAMERA);
 
             }
         });
@@ -76,6 +87,21 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
     //Si tot ha sortit bé
     if (RESULT_OK == resultCode) {
+        if (requestCode == REQ_CAMERA) {
+            //Bitmap bmp = new Bit
+
+            File f = new File(Environment.getExternalStorageDirectory()
+                    .toString());
+            for (File temp : f.listFiles()) {
+                if (temp.getName().equals("Items")) {
+                    f = temp;
+                    break;
+                }
+            }
+
+
+
+        /*
         // Obtenim els extras
         Bundle extras = data.getExtras();
         // Obtenim la imatge del extra
@@ -83,7 +109,48 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         //La establim en el widget
         imatge = (ImageView) findViewById(R.id.image);
-        imatge.setImageBitmap(bmp);
+        imatge.setImageBitmap(bmp);*/
+
+
+        //-----------------------------------------------------------
+        //super.onActivityResult(requestCode, resultCode, data);
+        //if (resultCode == Activity.RESULT_OK) {
+        //    if (requestCode == REQUEST_CAMERA) {
+        //        File f = new File(Environment.getExternalStorageDirectory()
+        //                .toString());
+        //        for (File temp : f.listFiles()) {
+        //            if (temp.getName().equals("temp.jpg")) {
+        //                f = temp;
+         //               break;
+         //           }
+         //       }
+                try {
+                    Bitmap bm;
+                    BitmapFactory.Options btmapOptions = new BitmapFactory.Options();
+
+                    bm = BitmapFactory.decodeFile(f.getAbsolutePath(), btmapOptions);
+
+                    bm = Bitmap.createScaledBitmap(bm, 70, 70, true);
+                    imatge.setImageBitmap(bm);
+                    uploadImagePath = f.getAbsolutePath();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }// else if (requestCode == SELECT_FILE) {
+                Uri selectedImageUri = data.getData();
+
+                String tempPath = getPath(selectedImageUri, getActivity());
+                Bitmap bm;
+                BitmapFactory.Options btmapOptions = new BitmapFactory.Options();
+                bm = BitmapFactory.decodeFile(tempPath, btmapOptions);
+                imatge = (ImageView) findViewById(R.id.image);
+                imatge.setImageBitmap(bm);
+                uploadImagePath = tempPath;
+
+            }
+        }
+
     }
-}
-}
+
+
